64ed67f95f40212cab28d3ec165060ed8ecb7db9
Henni <henniyassine.enpo@gmail.com>
2021-10-28 13:32:33 +0200

    refactored initization + populating example !

--- src/main/java/src/Model/Alegro.java
+++ src/main/java/src/Model/Alegro.java
@@ -1,16 +1,58 @@
 package src.Model;
 
+import com.franz.agraph.repository.AGCatalog;
+import com.franz.agraph.repository.AGRepository;
+import com.franz.agraph.repository.AGRepositoryConnection;
 import com.franz.agraph.repository.AGServer;
 
 public class Alegro {
-    static public final String SUPER_USERNAME = "YassineAtos";
-    static public final String SUPER_PASSWORD ="YassineAtosPwd";
-    public static final String SERVER_URL = "http://localhost:10035";
-    private static final String CATALOG_ID = "scratch";
-    private static final String REPOSITORY_ID = "javatutorial";
-    static public final String USERNAME = "TestUser";
-    static public final String PASSWORD = "TestUserPwd";
+    //static public final String SUPER_USERNAME = "YassineAtos";
+    //static public final String SUPER_PASSWORD ="YassineAtosPwd";
+    //public static final String SERVER_URL = "http://localhost:10035";
+    //private static final String CATALOG_ID = "scratch";
+    //private static final String REPOSITORY_ID = "javatutorial";
+    //static public final String USERNAME = "TestUser";
+    //static public final String PASSWORD = "TestUserPwd";
+    private  String SERVER_URL;
+    private String USERNAME;
+    private String PASSWORD;
+    private  String CATALOG_ID;
+    private  String REPOSITORY_ID = "testRepo";
+    private AGCatalog catalog;
+    private AGRepository repo;
+    private static AGRepositoryConnection conn;
 
+    public AGRepositoryConnection init () throws Exception{
+        if(this.conn == null ){
+            if(SERVER_URL.isEmpty() || USERNAME.isEmpty() || PASSWORD.isEmpty() ||
+                    SERVER_URL.isBlank() || USERNAME.isBlank() || PASSWORD.isBlank() )
+                {
+                throw new Exception("Non valid Connection Parameters  ! ");
+                }
+
+            AGServer server = new AGServer(this.SERVER_URL, this.USERNAME, this.PASSWORD);
+            this.catalog = server.getRootCatalog();
+            this.repo =  catalog.createRepository(this.REPOSITORY_ID);
+            this.repo.initialize();
+            this.conn = repo.getConnection();
+            System.out.println("Connected succesfully, Repository : "+ repo.getRepositoryID()+ ", it constains : "+conn.size() );
+
+        }
+        return this.conn;
+    }
+
+
+    public void setSERVER_URL(String SERVER_URL) {
+        this.SERVER_URL = SERVER_URL;
+    }
+
+    public void setUSERNAME(String USERNAME) {
+        this.USERNAME = USERNAME;
+    }
+
+    public void setPASSWORD(String PASSWORD) {
+        this.PASSWORD = PASSWORD;
+    }
 
 
 }
--- src/main/java/src/application.java
+++ src/main/java/src/application.java
@@ -1,29 +1,54 @@
 package src;
 
-import com.franz.agraph.repository.AGCatalog;
-import com.franz.agraph.repository.AGRepository;
-import com.franz.agraph.repository.AGRepositoryConnection;
-import com.franz.agraph.repository.AGServer;
+import com.franz.agraph.repository.*;
+import com.hp.hpl.jena.sparql.resultset.XMLInput;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.vocabulary.XMLSchema;
 import src.Model.Alegro;
 
 
 
 public class application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+    final String USERNAME  ="TestUser";
+    final String PASSWORD = "TestUserPwd";
+    final String URL_SERVER ="http://localhost:10035";
+    String human_ns = "http://www.inria.fr/2015/humans#";
+    String humanInstance_ns = human_ns+"-instances# ";
 
-        // AGServer server = new AGServer(Alegro.SERVER_URL, Alegro.SUPER_USERNAME, Alegro.SUPER_PASSWORD);
-        AGServer server = new AGServer(Alegro.SERVER_URL, Alegro.USERNAME, Alegro.PASSWORD);
-        //System.out.println(server.listCatalogs());
-        AGCatalog catalog = server.getRootCatalog();
-        // System.out.println(catalog.getCatalogName());
-        //  System.out.println(catalog.listRepositories());
-        AGRepository repo =  catalog.createRepository("testRepo");
-        repo.initialize();
-        System.out.println(repo.isWritable());
+    Alegro alegro = new Alegro();
+    alegro.setSERVER_URL(URL_SERVER);
+    alegro.setUSERNAME(USERNAME);
+    alegro.setPASSWORD(PASSWORD);
+    AGRepositoryConnection conn = alegro.init();
+    AGValueFactory vf =  conn.getValueFactory();
 
-        AGRepositoryConnection conn = repo.getConnection();
-        //closeBeforeExit(conn);
-        System.out.println("My repository : "+ repo.getRepositoryID()+ ", it constains : "+conn.size() );
+    // inserting individuals
+        IRI alice_exemple = vf.createIRI(humanInstance_ns,"alice_exemple");
+        IRI bob_exemple = vf.createIRI(humanInstance_ns,"bob_exemple");
+        IRI name = vf.createIRI(human_ns,"name");
+        IRI age = vf.createIRI(human_ns,"age");
+
+        Literal alice_exemple_name  = vf.createLiteral("Alice_Exemple");
+        Literal bob_exemple_name = vf.createLiteral("Bob_Exemple");
+
+        Literal alice_exemple_age  = vf.createLiteral("22", XMLSchema.INT);
+        Literal bob_exemple_age = vf.createLiteral("23", XMLSchema.INT);
+
+        System.out.println("before insert : "+conn.size());
+        // even if the subject URI exists alegro will still add it ! beware if needed verify that the triple doesn't exists already !
+        conn.add(alice_exemple,name,alice_exemple_name);
+        conn.add(bob_exemple,name,bob_exemple_age);
+
+        //2nd way to populate via statements
+        Statement stmnt1 = vf.createStatement(alice_exemple,age,alice_exemple_age);
+        Statement stmnt2 = vf.createStatement(bob_exemple,age,bob_exemple_age);
+        conn.add(stmnt1);
+        conn.add(stmnt2);
+        System.out.println("After insert : "+conn.size());
+        conn.close();
 
     }
 }
